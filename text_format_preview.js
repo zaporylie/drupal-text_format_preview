9fbf4c94c4a6888c3506583f775e0be3f0601970
zaporylie <jakub@nymedia.no>
2015-02-18 23:07:44 +0100

    Code clean and inline comments

--- text_format_preview.js
+++ text_format_preview.js
@@ -5,6 +5,7 @@
 
         // Store this as variable.
         var checkbox = $(this);
+
         // Store wrapper as variable.
         var wrapper = checkbox.parents('.text-format-preview-wrapper');
 
@@ -15,6 +16,8 @@
         else {
           wrapper.find('.text-format-preview-preview').hide();
         }
+
+        // Show or hide element on checkbox state change.
         checkbox.change(function() {
           // Show only if autopreview checkbox is checked.
           if (checkbox.is(':checked')) {
@@ -28,11 +31,13 @@
         // Preview autoupdate.
         wrapper.find('textarea').bind('change keyup', function() {
           if (checkbox.is(':checked')) {
+            // Clear execution delay.
             window.clearTimeout(checkbox.data('timeout'));
+
+            // Execute after 300ms.
             checkbox.data('timeout', setTimeout(function () {
+              // Only if last request is not in progress.
               if (Drupal.ajax[checkbox.attr('id')].ajaxing == false || Drupal.ajax[checkbox.attr('id')].ajaxing == undefined) {
-                // Send new request after 300ms and if Drupal is not processing
-                // last one.
                 Drupal.ajax[checkbox.attr('id')].eventResponse(checkbox, 'click');
               }
             }, 300));
@@ -44,14 +49,16 @@
 
   Drupal.ajax.prototype.commands.text_format_preview = function(ajax, response, status) {
 
+    // Get default selector for triggering element.
     if (response.selector == null || response.selector == undefined) {
       response.selector = ajax.selector;
     }
 
     var trigger = $(response.selector);
     var wrapper = trigger.parents('.text-format-preview-wrapper');
+
+    // Put output to preview container.
     wrapper.find('.text-format-preview-preview').html(response.preview);
   };
 
 })(jQuery);
-
